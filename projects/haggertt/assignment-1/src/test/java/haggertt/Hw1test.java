38aab4e9856ec3f1f2cbb6cf0aa6a78d34b54749
haggertt <tyhaggerty@gmail.com>
2017-04-16 16:00:19 -0700

    Adding Evosuite Tests

---
+++ projects/haggertt/assignment-1/src/test/java/haggertt/Hw1test.java
@@ -0,0 +1,38 @@
+package haggertt;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class Hw1test
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public Hw1test( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( Hw1test.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testHw1()
+    {
+        assertTrue( true );
+    }
+}
